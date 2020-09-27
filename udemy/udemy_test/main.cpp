a5018eaa5dfb85b1346436c19358fd0814ed39c8
Mykhailo418 <smmichael418@gmail.com>
2020-09-27 23:44:34 +0300

    qProgressBar

--- udemy/udemy_test/main.cpp
+++ udemy/udemy_test/main.cpp
@@ -2,6 +2,11 @@
 
 #include <QApplication>
 #include <QPushButton>
+#include <QFont>
+#include <QProgressBar>
+
+QPushButton* createPushButton();
+QProgressBar* createProgressbar();
 
 int main(int argc, char *argv[])
 {
@@ -9,8 +14,27 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-    QPushButton btn("QPushButton Button");
-    btn.show();
+    QPushButton* btn = createPushButton();
+    QProgressBar* proggressBar = createProgressbar();
+    btn->show();
+    proggressBar->show();
 
     return a.exec();
+}
+
+QPushButton* createPushButton()
+{
+    QPushButton* btn = new QPushButton("QPushButton Button");
+    btn->setText("new caption");
+    btn->setToolTip("tooltip");
+    QFont MyFont("Arial", QFont::Bold, true);
+    btn->setFont(MyFont);
+    return btn;
+}
+
+QProgressBar* createProgressbar()
+{
+    QProgressBar* progressbar = new QProgressBar();
+    progressbar->setValue(50);
+    return progressbar;
 }
